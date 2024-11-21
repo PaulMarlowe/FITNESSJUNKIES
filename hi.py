2b4a8b14f06542ff72f82098150ddda4ed3c39bb
anniwinogradoff <awinogradoff001@student.franciscan.edu>
2024-11-21 11:25:23 -0500

    done

--- hi.py
+++ hi.py
@@ -1,3 +1,54 @@
 print("Hey")
 print("Can You see me")
 print("We are the coolest group")
+
+#lifitng app
+#separate muscles into groups:
+   #arms
+   #shoulders
+   #back
+   #chest
+   #core
+   #legs
+#pick which muscles they want to target
+#choose how many days you work out a week: 2, 3, 4, 5, 6
+
+#input hypertrophy...increasing weight and lower reps
+#include reps and sets for 
+
+#three mainpoints
+   #tkinter window for choosing muscle group
+   #user input
+   #learning to storing user input info could be a new main point. We could also create a plot
+
+#arms
+   #rope extentions
+   #tricep dips
+   #preacher curls
+   #barbell curls
+   
+#back
+   #rows
+   #lat pull downs
+   #reverse flys 
+
+#shoulder
+   #overhead press
+   #lateral raises
+   #face pulls
+
+#chest
+   #bench press
+   #pec flys
+   #push-ups
+
+#legs
+   #hamstring curls
+   #back squat
+   #calf raises
+   #leg press
+   #legs extensions
+
+
+#functions that we would need: 
+   #class with methods that 
